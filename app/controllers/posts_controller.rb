a83be2ff777d4ceffd4ef021298d8bc15f7e932b
Robert Hughes <roberts.ginger.email@gmail.com>
2018-04-21 20:46:49 +0100

    custom filter routes added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.all
   end
@@ -28,7 +27,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-private
+  private
 
   def post_params
     params.require(:post).permit(:title, :desription, :post_status, :author_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
   resources :authors, only: [:show]
+  get 'authors/:id/posts', to: 'authors#posts_index'
+  get 'authors/:id/posts/:post_id', to: 'authors#post'
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts, only: %i[index show new create edit update]
 
   root 'posts#index'
 end
